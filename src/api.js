e3b8aea57d35a93747619cd00572083d8535aef5
Nodir_442 <127777677+Nodir442@users.noreply.github.com>
2025-02-07 13:52:29 +0500

    Update api.js
    Simulated data rendering(1-second)

--- src/api.js
+++ src/api.js
@@ -4,13 +4,13 @@ export function fakeFetchCrypto() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(cryptoData);
-    }, 1);
+    }, 1000);
   });
 }
 export function fetchAssets() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(cryptoAssets);
-    }, 1);
+    }, 1000);
   });
 }
